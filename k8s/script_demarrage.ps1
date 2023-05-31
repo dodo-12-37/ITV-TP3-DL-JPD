63e24af7aa01aab7e6905fc9a14f21c1a68917b6
Dominique Lebrun <dodo-12-37@hotmail.com>
2023-05-31 18:52:07 -0400

    Paufiner script

--- k8s/script_demarrage.ps1
+++ k8s/script_demarrage.ps1
@@ -1,14 +1,15 @@
-$Namespace = "unitaire"
+$Namespace = "NAMESPACE"
+# $NodePort = NODEPORT
+$NodePort = 4441919
 
 
-
-$fileData = (Get-Content .\test\unitaire.yaml) -replace $Namespace, “production”
-$fileData | Set-Content .\test\production.yaml
+$fileData = (Get-Content .\unitaire\nouveauConfig.yaml) -creplace $Namespace, “production” -creplace $NodePort, "30002"
+$fileData | Set-Content .\unitaire\nouveauProduction.yaml
 $fileData | kubectl apply -f - 
 
 # $fileData = Get-Content .\test\deploy.yaml
 # $fileData | kubectl apply -f -
-kubectl apply -f .\test\deploy.yaml --namespace=production
+# kubectl apply -f .\test\deploy.yaml --namespace=production
 
 
 write-host "Terminer"
